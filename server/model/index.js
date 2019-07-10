5b5d8a2143ec5e2ba03fb67612915e74e38cd7dc
Peace Oyedeji <peaceoyedeji@Peaces-MacBook-Pro.local>
2019-07-10 21:56:22 +0100

    get all products of a department create route to get all products of a department create controller for route write test to test for functionality of implementation

--- server/model/index.js
+++ server/model/index.js
@@ -1,11 +1,52 @@
+// const Sequelize = require('sequelize');
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// const env = process.env.NODE_ENV || 'development';
+// const config = require('../config/config.js')[env];
+
+// const db = new Sequelize(config.url);
+
+// export default db;
+
+
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
-const dotenv = require('dotenv');
 
-dotenv.config();
-
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.js')[env];
 
-const db = new Sequelize(config.url);
+const db = {};
+const opts = {
+  dialectOptions: {
+    multipleStatements: true
+  }
+};
 
-export default db;
+opts.logging = env === 'test';
+const sequelize = new Sequelize(config.url, opts);
+
+
+fs
+  .readdirSync(__dirname)
+  .filter((file) => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
